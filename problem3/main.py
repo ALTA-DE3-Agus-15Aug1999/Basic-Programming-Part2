2e7383dd13687f7dd86d10551035988591eb1496
agusfn15 <nagusfernando@gmail.com>
2024-05-14 13:57:42 +0700

    solve problem 3

--- problem3/main.py
+++ problem3/main.py
@@ -4,3 +4,13 @@ r = 4.0
 
 
 # kode disini
+T = int(T)
+r = int(r)
+
+if r % 7 == 0:
+    phi = 22/7
+else :
+    phi = 3.14
+
+Lp = (2 * phi * r * (r + T) )
+print("Luas Permukaan Tabung : ", (Lp))
